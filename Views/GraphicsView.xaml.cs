4f0d7711a3978eeebb97faf5be80b36313cd9224
kallDun <igorxlonline24@gmail.com>
2023-11-19 14:37:13 +0200

    added simple grid to graphics view

--- Views/GraphicsView.xaml.cs
+++ Views/GraphicsView.xaml.cs
@@ -1,28 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace BadassUniverse_MapEditor.Views
 {
-    /// <summary>
-    /// Interaction logic for GraphicsView.xaml
-    /// </summary>
     public partial class GraphicsView : UserControl
     {
         public GraphicsView()
         {
             InitializeComponent();
+            InitGrid();
+        }
+
+        private void InitGrid()
+        {
+            int rows = 40;
+            int cols = 80;
+            int size = 24;
+            for (int i = 0; i < rows; i++)
+            {
+                RowDefinition row = new RowDefinition();
+                row.Height = new GridLength(size);
+                GraphicsGrid.RowDefinitions.Add(row);
+            }
+            for (int i = 0; i < cols; i++)
+            {
+                ColumnDefinition col = new ColumnDefinition();
+                col.Width = new GridLength(size);
+                GraphicsGrid.ColumnDefinitions.Add(col);
+            }
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Rectangle rect = new Rectangle();
+                    rect.Fill = Brushes.Gray;
+                    rect.Stroke = Brushes.White;
+                    rect.StrokeThickness = 1;
+                    rect.Width = size;
+                    rect.Height = size;
+                    Grid.SetRow(rect, i);
+                    Grid.SetColumn(rect, j);
+                    GraphicsGrid.Children.Add(rect);
+                }
+            }
         }
     }
 }
