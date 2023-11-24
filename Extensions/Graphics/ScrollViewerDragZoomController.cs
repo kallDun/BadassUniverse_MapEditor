e72bb8d893235304b87ee3db4784ade3a7a4fbf4
kallDun <igorxlonline24@gmail.com>
2023-11-25 01:53:59 +0200

    move drag & zoom logic to separate utility class & change input on custom mouse button

---
+++ Extensions/Graphics/ScrollViewerDragZoomController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace BadassUniverse_MapEditor.Extensions.Graphics
+{
+    public class ScrollViewerDragZoomController
+    {
+        public ScrollViewerDragZoomController(ScrollViewer scrollViewer, 
+            FrameworkElement content, ScaleTransform scaleTransform, MouseButton dragMouseButton)
+        {
+            this.scrollViewer = scrollViewer;
+            this.content = content;
+            this.scaleTransform = scaleTransform;
+            this.dragMouseButton = dragMouseButton;
+
+            scrollViewer.ScrollChanged += OnScrollViewerScrollChanged;
+            scrollViewer.PreviewMouseWheel += OnPreviewMouseWheel;
+            scrollViewer.PreviewMouseUp += OnMouseButtonUp;
+            scrollViewer.MouseUp += OnMouseButtonUp;
+            scrollViewer.PreviewMouseDown += OnMouseButtonDown;
+            scrollViewer.MouseDown += OnMouseButtonDown;
+            scrollViewer.MouseMove += OnMouseMove;
+        }
+
+        private ScrollViewer scrollViewer;
+        private FrameworkElement content;
+        private ScaleTransform scaleTransform;
+        private MouseButton dragMouseButton;
+
+        private Point? lastCenterPositionOnTarget;
+        private Point? lastMousePositionOnTarget;
+        private Point? lastDragPoint;
+        private double zoomValue = 1;
+        private double zoomMin = 1;
+        private double zoomMax = 10;
+        private double zoomDelta = .5f;
+
+        private void OnMouseMove(object sender, MouseEventArgs e)
+        {
+            if (lastDragPoint.HasValue)
+            {
+                Point posNow = e.GetPosition(scrollViewer);
+
+                double dX = posNow.X - lastDragPoint.Value.X;
+                double dY = posNow.Y - lastDragPoint.Value.Y;
+                lastDragPoint = posNow;
+
+                scrollViewer.ScrollToHorizontalOffset(scrollViewer.HorizontalOffset - dX);
+                scrollViewer.ScrollToVerticalOffset(scrollViewer.VerticalOffset - dY);
+            }
+        }
+
+        private void OnMouseButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == dragMouseButton && e.ButtonState == MouseButtonState.Pressed)
+            {
+                var mousePos = e.GetPosition(scrollViewer);
+                if (mousePos.X <= scrollViewer.ViewportWidth && mousePos.Y <
+                    scrollViewer.ViewportHeight) //make sure we still can use the scrollbars
+                {
+                    scrollViewer.Cursor = Cursors.SizeAll;
+                    lastDragPoint = mousePos;
+                    Mouse.Capture(scrollViewer);
+                }
+            }
+        }
+
+        private void OnMouseButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == dragMouseButton && e.ButtonState == MouseButtonState.Released)
+            {
+                scrollViewer.Cursor = Cursors.Arrow;
+                scrollViewer.ReleaseMouseCapture();
+                lastDragPoint = null;
+            }
+        }
+
+        private void OnPreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            lastMousePositionOnTarget = Mouse.GetPosition(content);
+
+            if (e.Delta > 0)
+            {
+                ChangeZoom(zoomValue + zoomDelta);
+            }
+            if (e.Delta < 0)
+            {
+                ChangeZoom(zoomValue - zoomDelta);
+            }
+
+            e.Handled = true;
+        }
+
+        private void ChangeZoom(double value)
+        {
+            double newValue = Math.Clamp(value, zoomMin, zoomMax);
+            if (newValue == zoomValue) return;
+            zoomValue = newValue;
+            scaleTransform.ScaleX = zoomValue;
+            scaleTransform.ScaleY = zoomValue;
+
+            var centerOfViewport = new Point(scrollViewer.ViewportWidth / 2, scrollViewer.ViewportHeight / 2);
+            lastCenterPositionOnTarget = scrollViewer.TranslatePoint(centerOfViewport, content);
+
+            scrollViewer.ScrollToHorizontalOffset(scrollViewer.HorizontalOffset);
+            scrollViewer.ScrollToVerticalOffset(scrollViewer.VerticalOffset);
+        }
+
+        private void OnScrollViewerScrollChanged(object sender, ScrollChangedEventArgs e)
+        {
+            if (e.ExtentHeightChange != 0 || e.ExtentWidthChange != 0)
+            {
+                Point? targetBefore = null;
+                Point? targetNow = null;
+
+                if (!lastMousePositionOnTarget.HasValue)
+                {
+                    if (lastCenterPositionOnTarget.HasValue)
+                    {
+                        var centerOfViewport = new Point(scrollViewer.ViewportWidth / 2, scrollViewer.ViewportHeight / 2);
+                        Point centerOfTargetNow = scrollViewer.TranslatePoint(centerOfViewport, content);
+
+                        targetBefore = lastCenterPositionOnTarget;
+                        targetNow = centerOfTargetNow;
+                    }
+                }
+                else
+                {
+                    targetBefore = lastMousePositionOnTarget;
+                    targetNow = Mouse.GetPosition(content);
+
+                    lastMousePositionOnTarget = null;
+                }
+
+                if (targetBefore.HasValue)
+                {
+                    double dXInTargetPixels = targetNow.Value.X - targetBefore.Value.X;
+                    double dYInTargetPixels = targetNow.Value.Y - targetBefore.Value.Y;
+
+                    double multiplicatorX = e.ExtentWidth / content.ActualWidth;
+                    double multiplicatorY = e.ExtentHeight / content.ActualHeight;
+
+                    double newOffsetX = scrollViewer.HorizontalOffset - dXInTargetPixels * multiplicatorX;
+                    double newOffsetY = scrollViewer.VerticalOffset - dYInTargetPixels * multiplicatorY;
+
+                    if (double.IsNaN(newOffsetX) || double.IsNaN(newOffsetY)) return;
+
+                    scrollViewer.ScrollToHorizontalOffset(newOffsetX);
+                    scrollViewer.ScrollToVerticalOffset(newOffsetY);
+                }
+            }
+        }
+    }
+
+    public static class ScrollViewerDragZoomControllerExtensions
+    {
+        public static ScrollViewerDragZoomController EnableDragZoom(this ScrollViewer scrollViewer, 
+            FrameworkElement content, ScaleTransform contentScaleTransform, MouseButton dragMouseButton)
+        {
+            return new ScrollViewerDragZoomController(scrollViewer, content, contentScaleTransform, dragMouseButton);
+        }
+    }
+}
--- Views/GraphicsView.xaml.cs
+++ Views/GraphicsView.xaml.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.Diagnostics;
+﻿using BadassUniverse_MapEditor.Extensions.Graphics;
+using System;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -10,26 +10,11 @@ namespace BadassUniverse_MapEditor.Views
 {
     public partial class GraphicsView : UserControl
     {
-        Point? lastCenterPositionOnTarget;
-        Point? lastMousePositionOnTarget;
-        Point? lastDragPoint;
-        double zoomValue = 1;
-        double zoomMin = 1;
-        double zoomMax = 10;
-        double zoomDelta = .5f;
-
         public GraphicsView()
         {
             InitializeComponent();
             InitGrid();
-
-            scrollViewer.ScrollChanged += OnScrollViewerScrollChanged;
-            scrollViewer.MouseLeftButtonUp += OnMouseLeftButtonUp;
-            scrollViewer.PreviewMouseLeftButtonUp += OnMouseLeftButtonUp;
-            scrollViewer.PreviewMouseWheel += OnPreviewMouseWheel;
-
-            scrollViewer.PreviewMouseLeftButtonDown += OnMouseLeftButtonDown;
-            scrollViewer.MouseMove += OnMouseMove;
+            scrollViewer.EnableDragZoom(GraphicsGrid, scaleTransform, MouseButton.Middle);
         }
 
         private void InitGrid()
@@ -77,115 +62,6 @@ namespace BadassUniverse_MapEditor.Views
         }
 
 
-        void OnMouseMove(object sender, MouseEventArgs e)
-        {
-            if (lastDragPoint.HasValue)
-            {
-                Point posNow = e.GetPosition(scrollViewer);
-
-                double dX = posNow.X - lastDragPoint.Value.X;
-                double dY = posNow.Y - lastDragPoint.Value.Y;
-
-                lastDragPoint = posNow;
-
-                scrollViewer.ScrollToHorizontalOffset(scrollViewer.HorizontalOffset - dX);
-                scrollViewer.ScrollToVerticalOffset(scrollViewer.VerticalOffset - dY);
-            }
-        }
-
-        void OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            var mousePos = e.GetPosition(scrollViewer);
-            if (mousePos.X <= scrollViewer.ViewportWidth && mousePos.Y <
-                scrollViewer.ViewportHeight) //make sure we still can use the scrollbars
-            {
-                scrollViewer.Cursor = Cursors.SizeAll;
-                lastDragPoint = mousePos;
-                Mouse.Capture(scrollViewer);
-            }
-        }
-
-        void OnPreviewMouseWheel(object sender, MouseWheelEventArgs e)
-        {
-            lastMousePositionOnTarget = Mouse.GetPosition(GraphicsGrid);
-
-            if (e.Delta > 0)
-            {
-                ChangeZoom(zoomValue + zoomDelta);
-            }
-            if (e.Delta < 0)
-            {
-                ChangeZoom(zoomValue - zoomDelta);
-            }
-
-            e.Handled = true;
-        }
-
-        void OnMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
-        {
-            scrollViewer.Cursor = Cursors.Arrow;
-            scrollViewer.ReleaseMouseCapture();
-            lastDragPoint = null;
-        }
-
-        void ChangeZoom(double value)
-        {
-            double newValue = Math.Clamp(value, zoomMin, zoomMax);
-            if (newValue == zoomValue) return;
-            zoomValue = newValue;
-            scaleTransform.ScaleX = zoomValue;
-            scaleTransform.ScaleY = zoomValue;
-
-            var centerOfViewport = new Point(scrollViewer.ViewportWidth / 2, scrollViewer.ViewportHeight / 2);
-            lastCenterPositionOnTarget = scrollViewer.TranslatePoint(centerOfViewport, GraphicsGrid);
-
-            scrollViewer.ScrollToHorizontalOffset(scrollViewer.HorizontalOffset);
-            scrollViewer.ScrollToVerticalOffset(scrollViewer.VerticalOffset);
-        }
-
-        void OnScrollViewerScrollChanged(object sender, ScrollChangedEventArgs e)
-        {
-            if (e.ExtentHeightChange != 0 || e.ExtentWidthChange != 0)
-            {
-                Point? targetBefore = null;
-                Point? targetNow = null;
-
-                if (!lastMousePositionOnTarget.HasValue)
-                {
-                    if (lastCenterPositionOnTarget.HasValue)
-                    {
-                        var centerOfViewport = new Point(scrollViewer.ViewportWidth / 2, scrollViewer.ViewportHeight / 2);
-                        Point centerOfTargetNow = scrollViewer.TranslatePoint(centerOfViewport, GraphicsGrid);
-
-                        targetBefore = lastCenterPositionOnTarget;
-                        targetNow = centerOfTargetNow;
-                    }
-                }
-                else
-                {
-                    targetBefore = lastMousePositionOnTarget;
-                    targetNow = Mouse.GetPosition(GraphicsGrid);
-
-                    lastMousePositionOnTarget = null;
-                }
-
-                if (targetBefore.HasValue)
-                {
-                    double dXInTargetPixels = targetNow.Value.X - targetBefore.Value.X;
-                    double dYInTargetPixels = targetNow.Value.Y - targetBefore.Value.Y;
-
-                    double multiplicatorX = e.ExtentWidth / GraphicsGrid.ActualWidth;
-                    double multiplicatorY = e.ExtentHeight / GraphicsGrid.ActualHeight;
-
-                    double newOffsetX = scrollViewer.HorizontalOffset - dXInTargetPixels * multiplicatorX;
-                    double newOffsetY = scrollViewer.VerticalOffset - dYInTargetPixels * multiplicatorY;
-
-                    if (double.IsNaN(newOffsetX) || double.IsNaN(newOffsetY)) return;
-
-                    scrollViewer.ScrollToHorizontalOffset(newOffsetX);
-                    scrollViewer.ScrollToVerticalOffset(newOffsetY);
-                }
-            }
-        }
+        
     }
 }
