360a6468d15443d338074c8e6e2728d61f980aa1
BulatNick <bulatnikov.mykyta@vu.cdu.edu.ua>
2023-11-25 17:03:57 +0200

    selection view UI done, property view sub elements created, list to do

---
+++ Views/Elements/ItemElementView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace BadassUniverse_MapEditor.Views.Elements
+{
+    /// <summary>
+    /// Interaction logic for ItemElementView.xaml
+    /// </summary>
+    public partial class ItemElementView : UserControl
+    {
+        public ItemElementView()
+        {
+            InitializeComponent();
+        }
+    }
+}
