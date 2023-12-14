dcfceaaa682059acc8a82ffdb4590008bf0ca59a
kallDun <igorxlonline24@gmail.com>
2023-12-14 03:32:22 +0200

    make drawing line from hardcode to formula

--- Views/Elements/GraphicsCellView.cs
+++ Views/Elements/GraphicsCellView.cs
@@ -300,19 +300,31 @@ public class GraphicsCellView
         }
     }
 
-    private void InitFacadeBackground(List<Color> colors)
+    private void InitFacadeBackground(IReadOnlyList<Color> colors)
     {
-        Color GetColor(int lineIndex, int totalLines = 5)
+        const int linesCount = 5; // NOT EVEN NUMBER
+        
+        Color GetColor(int lineIndex)
         {
-            int index = lineIndex * colors.Count / totalLines;
+            int index = lineIndex * colors.Count / linesCount;
             return colors[index];
         }
 
-        AddLineToContent(0 * size, 0.6 * size, 0.4 * size, 1 * size, GetColor(0));
-        AddLineToContent(0 * size, 0.3 * size, 0.7 * size, 1 * size, GetColor(1));
-        AddLineToContent(0 * size, 0 * size, 1 * size, 1 * size, GetColor(2));
-        AddLineToContent(0.3 * size, 0 * size, 1 * size, 0.7 * size, GetColor(3));
-        AddLineToContent(0.6 * size, 0 * size, 1 * size, 0.4 * size, GetColor(4));
+        int dividedInto = (linesCount - 1) / 2;
+        float part = 1f / (dividedInto + 1);
+        for (int i = 0; i < dividedInto; i++)
+        {
+            var currPart = part * (i + 1);
+            var currPart2 = 1 - currPart;
+            AddLineToContent(0 * size, currPart2 * size, currPart * size, 1 * size, GetColor(i));
+        }
+        AddLineToContent(0 * size, 0 * size, 1 * size, 1 * size, GetColor(dividedInto));
+        for (int i = 0; i < dividedInto; i++)
+        {
+            var currPart = part * (i + 1);
+            var currPart2 = 1 - currPart;
+            AddLineToContent(currPart * size, 0 * size, 1 * size, currPart2 * size, GetColor(i + dividedInto + 1));
+        }
 
         AddBorderToContent();
     }
